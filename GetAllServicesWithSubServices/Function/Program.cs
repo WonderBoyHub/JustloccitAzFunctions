29196e03b28f879c46893a727aae20abfabff85c
WonderBoyHub <prawn_diner.0d@icloud.com>
2025-05-06 20:01:31 +0200

    Update Program.cs

--- GetAllServicesWithSubServices/Function/Program.cs
+++ GetAllServicesWithSubServices/Function/Program.cs
@@ -5,6 +5,7 @@ var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
+
 // Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
 // builder.Services
 //     .AddApplicationInsightsTelemetryWorkerService()
